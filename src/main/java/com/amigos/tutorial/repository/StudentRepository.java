bb527a24d460563fd8793eafde4fdaba2a31afc1
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-13 16:20:00 +0700

    business logic

--- src/main/java/com/amigos/tutorial/repository/StudentRepository.java
+++ src/main/java/com/amigos/tutorial/repository/StudentRepository.java
@@ -2,9 +2,16 @@ package com.amigos.tutorial.repository;
 
 import com.amigos.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // SELECT * FROM student WHERE email = ?
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
