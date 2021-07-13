6b2acbf15d43e0df16a3599eeac8255a198bd630
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-13 14:53:56 +0700

    JPA repository

---
+++ src/main/java/com/amigos/tutorial/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.amigos.tutorial.repository;
+
+import com.amigos.tutorial.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
