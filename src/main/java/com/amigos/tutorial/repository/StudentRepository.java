89908bac543dc7c92be0673cde9265fcfeeacea9
R. Fadhil <ridhanfadhilah@gmail.com>
2021-07-13 18:02:53 +0700

    repository

--- src/main/java/com/amigos/tutorial/repository/StudentRepository.java
+++ src/main/java/com/amigos/tutorial/repository/StudentRepository.java
@@ -2,7 +2,6 @@ package com.amigos.tutorial.repository;
 
 import com.amigos.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
